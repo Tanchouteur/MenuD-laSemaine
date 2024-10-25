4944443493b0a455c7f2f5145c35c1b584630611
tanchou <louis.tanchou@gmail.com>
2024-10-25 08:30:21 +0200

    Rajout de poids et last used dans les model des produits brut

--- Sources/fr/tanchou/menudlasemaine/models/Legume.java
+++ Sources/fr/tanchou/menudlasemaine/models/Legume.java
@@ -1,10 +1,16 @@
 package fr.tanchou.menudlasemaine.models;
 
+import java.util.Date;
+
 public class Legume {
     private String legumeNom;
+    private int poids;
+    private Date lastUsed;
 
-    public Legume(String legumeNom) {
+    public Legume(String legumeNom, int poids, Date lastUsed) {
         this.legumeNom = legumeNom;
+        this.poids = poids;
+        this.lastUsed = lastUsed;
     }
 
     public String getLegumeNom() {
@@ -12,5 +18,21 @@ public class Legume {
     }
     public void setLegumeNom(String legumeNom) {
         this.legumeNom = legumeNom;
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    public Date getLastUsed() {
+        return lastUsed;
+    }
+
+    public void setLastUsed(Date lastUsed) {
+        this.lastUsed = lastUsed;
     }
 }
--- Sources/fr/tanchou/menudlasemaine/models/Viande.java
+++ Sources/fr/tanchou/menudlasemaine/models/Viande.java
@@ -1,10 +1,16 @@
 package fr.tanchou.menudlasemaine.models;
 
+import java.util.Date;
+
 public class Viande {
     private String nom;
+    private int poids;
+    private Date lastUsed;
 
-    public Viande(String nom) {
+    public Viande(String nom, int poids, Date lastUsed) {
         this.nom = nom;
+        this.poids = poids;
+        this.lastUsed = lastUsed;
     }
 
     public String getNomViande() {
@@ -13,5 +19,21 @@ public class Viande {
 
     public void setNomViande(String nom) {
         this.nom = nom;
+    }
+
+    public int getPoids() {
+        return poids;
+    }
+
+    public void setPoids(int poids) {
+        this.poids = poids;
+    }
+
+    public Date getLastUsed() {
+        return lastUsed;
+    }
+
+    public void setLastUsed(Date lastUsed) {
+        this.lastUsed = lastUsed;
     }
 }
