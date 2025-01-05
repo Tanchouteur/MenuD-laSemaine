baa743953fa80e1bbbf2460d31862854cc683de7
Tanchouteur <game.night.77176@gmail.com>
2025-01-05 13:37:57 +0000

    Java Docs

--- java/fr/tanchou/menudlasemaine/probabilitee/Incompatibilites.java
+++ java/fr/tanchou/menudlasemaine/probabilitee/Incompatibilites.java
@@ -2,15 +2,36 @@ package fr.tanchou.menudlasemaine.probabilitee;
 
 import fr.tanchou.menudlasemaine.menu.Produits;
 
+/**
+ * Represents an incompatibility between two products: a vegetable and a starch.
+ * <p>
+ * This class is used to model combinations of products that should not be paired together.
+ * </p>
+ */
 public class Incompatibilites {
+
+    /** The vegetable involved in the incompatibility. */
     private final Produits legume;
+
+    /** The starch involved in the incompatibility. */
     private final Produits feculent;
 
+    /**
+     * Constructs a new {@code Incompatibilites} instance with the specified vegetable and starch.
+     *
+     * @param legume   The vegetable that is incompatible with the starch.
+     * @param feculent The starch that is incompatible with the vegetable.
+     */
     public Incompatibilites(Produits legume, Produits feculent) {
         this.legume = legume;
         this.feculent = feculent;
     }
 
+    /**
+     * Returns a string representation of the incompatibility.
+     *
+     * @return A string describing the incompatibility between the vegetable and the starch.
+     */
     @Override
     public String toString() {
         return legume + " et " + feculent + " ne vont pas ensemble.";
